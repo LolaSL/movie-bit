e31ae0ad5f99ec4d270b9b595a056b06f4ea0e10
LolaSL <lolashepota@gmail.com>
2023-07-08 16:28:15 +0300

    Modify files:       modified:   src/pages/about/style.scss         modified:   vite.config.js

---
+++ .history/vite.config_20230708161854.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import React from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [React()],
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+}
+});
